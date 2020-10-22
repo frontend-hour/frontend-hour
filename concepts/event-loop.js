4252b6a620d93fb0073c4eef84fcda4d458ccebb
Raghu <konidelaraghu@gmail.com>
2020-10-22 23:02:42 +0100

    added event loop definition - WIP

--- concepts/event-loop.js
+++ concepts/event-loop.js
@@ -42,3 +42,7 @@ futureData.then(display);
 
   blockSomeTime();
   console.log('Me first');
+
+  // Event loop in JS is a mechanism of checking the Call Stack to see if there is any function that needs to run from Callback Queue
+
+  //i.e., The event loop on every iteration looks if there’s something in the call stack, and executes it until the call stack is empty.
