dbaa0a30980e38abd2c28fea861405e9a0e4de6c
MalladiChaitanya <chaitanya.kmc@gmail.com>
2020-11-02 22:40:34 +0530

    bubble sort

---
+++ algorithms/bubble-sort.js
@@ -0,0 +1,25 @@
+/*
+  Bubble sort works by comparing two adjacent numbers next to each other and then
+  swapping their places if the smaller index's value is larger than the larger
+  index's. Continue looping through until all values are in ascending order
+*/
+
+let nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+
+function bubbleSort(array) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while(swapped)
+    return array;
+}
+
+console.log(bubbleSort(nums));
