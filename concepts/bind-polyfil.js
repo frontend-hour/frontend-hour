9f41f37639a68640b1cecf3ebddda8b020182c6c
Raghu <konidelaraghu@gmail.com>
2020-11-19 15:44:55 +0530

    Bind polyfil

---
+++ concepts/bind-polyfil.js
@@ -0,0 +1,41 @@
+// Case 1 - No arguments
+
+const boundfn = fn.bind(ctx);
+
+const city = {
+    name: 'London',
+    printCityLove: function () {
+        console.log(`I love ${this.name} City.`)
+    }
+}
+setTimeout(city.printCityLove.bind(city), 1000);
+
+
+// case 2 - Passing arguments 
+
+const boundfn1 = fn.bind(ctx, arg1, arg2, arg3)
+
+const city1 = {
+    name: 'london',
+    printCityLove: function (personName) {
+        console.log(`${personName} loves ${this.name} city`);
+    }
+}
+
+setTimeout(city.printCityLove.bind(city, 'Raghu'), 100)
+
+
+
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function ([first, ...rest]) {
+        const context = this;
+        // const boundContext = arguments[0];
+        // const boundArgs = Array.from(arguments).slice(1);
+        // const boundArgs = arguments;
+        // boundArgs.shift();
+        return function (...args) {
+            const allArgs = rest.concat(args);
+            context.apply(first, allArgs);
+        }
+    }
+}
