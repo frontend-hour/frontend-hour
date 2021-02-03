32ec9254c413e0869e0e8779f831014659cd892e
Raghu <konidelaraghu@gmail.com>
2021-02-03 09:27:47 +0000

    Added recusion example

---
+++ concepts/recursion.js
@@ -0,0 +1,18 @@
+// A recursive function is a function that calls itself.
+
+// A recursive function always has a condition to stop calling itself, otherwise, it will call itself indefinitely. So a recursive function typically looks like the following:
+
+function recurse() {
+    if (condition) {
+        // stop calling itself
+        //...
+    } else {
+        recurse();
+    }
+}
+
+// Generally, recursive functions are used to break down a big problem into smaller ones. You can find that they are heavily used in the data structures like binary trees and graphs and algorithms such as binary search and quicksort.
+
+// Recursive function is clearly explained in below URL - 
+
+// https://www.javascripttutorial.net/javascript-recursive-function/
