a15a747680599fc74331e2180a8d70bfb6f6d03e
MalladiChaitanya <chaitanya.kmc@gmail.com>
2021-03-27 21:20:01 +0530

    Least disruptive substring

--- code-challenges/least-disruptive-path/least-disruptive-path.js
+++ code-challenges/least-disruptive-path/least-disruptive-path.js
@@ -1,8 +1,13 @@
 let a = [1, 2, 3, 4, 5, 6, 7, 8];
 let b = [3, 2, 3];
 
-function leastDisruptivePath() {
-
+function leastDisruptivePath(a, b) {
+    for(let k = 0; k < a.length; k ++) {
+        let score = 0;
+        for(let p = 0; p < b.length; p++) {
+            score = score + Math.abs(b[p] - a[k + p]);
+        }
+    }
 }
 
 leastDisruptivePath(a, b);
