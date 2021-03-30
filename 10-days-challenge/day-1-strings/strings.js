f39972e53afed4a6dc833e59aaa6ee093f5aa26f
Raghu <konidelaraghu@gmail.com>
2021-03-30 18:46:58 +0100

    Added longest common substring to Strings

--- 10-days-challenge/day-1-strings/strings.js
+++ 10-days-challenge/day-1-strings/strings.js
@@ -253,10 +253,38 @@ let arr = ["You", "are", "beautiful", "looking"]
 const asc = arr.sort((a, b) => a.length - b.length);
 // const decsending = arr.sort((a,b) => b.length - a.length);
 console.log(asc);
-  // console.log(decsending);
+// console.log(decsending);
 
 
-  //--------------------------------------------------------------------
+//--------------------------------------------------------------------
+// Longest common substring 
+//   Find the longest common string present in the given string 
 
+function longestCommonSubstring(str1, str2) {
+    let longestStr = '';
+    let longestStrArr = [];
+    let str;
+    let k = 1;
+    for (let i = 0; i < str1.length; i++) {
+        for (let j = 0; j < str2.length; j++) {
+            if (str1[i] === str2[j]) {
+                str = str1[i];
+                k = 1;
+                while ((i + k < str1.length) && (j + k < str2.length) && (str1[i + k] === str2[j + k])) {
+                    str += str1[i + k];
+                    ++k;
+                    console.log(str)
+                }
+                if (longestStr.length === 0 || (str.length > longestStr.length)) {
+                    longestStr = str;
+                }
+            }
+        }
+    }
+    return longestStr;
+}
+
+let substr = longestCommonSubstring('XYPXYXYZKREDDY', 'XYZKYXYPREDDY');
+console.log(substr);
 
 
