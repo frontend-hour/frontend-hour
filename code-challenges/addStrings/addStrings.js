6c6a3f2c4615571e82258a71897fed34fa1a3a1e
Raghu <konidelaraghu@gmail.com>
2021-04-13 08:11:48 +0100

    Added incomplete solution - added strings

--- code-challenges/addStrings/addStrings.js
+++ code-challenges/addStrings/addStrings.js
@@ -1,1 +1,17 @@
 // Add Solution
+// Incomplete solution - 
+var addStrings = function (num1, num2) {
+    let numStr1 = num1.split('').reverse();
+    let numStr2 = num2.split('').reverse();
+    let carry = 0;
+    let result = '';
+
+    for (let i = 0; i < numStr1.length; i++) {
+        let sum = Number(numStr1[i]) + Number(numStr2[i]) + carry;
+        if (sum > 9) {
+            carry = String(sum).charAt(0)
+        }
+        // sum = Number(carry) ? result.concat(String(sum).charAt(1) + Number(carry)) : result.concat(numStr1[i]+numStr2[i]);
+    }
+    return sum;
+};
