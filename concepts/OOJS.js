dffcbeb5264eecb938b869aeed7921d3b52a4d92
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2021-04-16 22:26:45 +0530

    Update OOJS.js

--- concepts/OOJS.js
+++ concepts/OOJS.js
@@ -29,6 +29,9 @@ let user3 = {
     }
 }
 
+// Instead of manually creating objects we created function called createUser
+// This function will return the object template with same properties and methods with different data
+
 /*- --------------------------------------*/
 
 function createUser(name, score) {
@@ -48,6 +51,14 @@ let user5 = createUser('raghu', 2);
 let user4 = createUser('myname', 2);
 
 /*- --------------------------------------*/
+// But the problem here is the methods that are being added to user objects is same for all users when created with createUser function
+// This will take extra unnecessary memory for users object.
+// So, in the next solution we will separate the common function accross user objects created using createUser function
+// And separated common methods are referenced in the objects.
+// For referencing the commong methods we are using Object.create this will point the newUser Object __proto__ to userFunctionStore
+// But the problem here is every time we are creating newUser object and adding properties.
+// All this work is done simply by using new keyword
+// new keyword when used before calling the createUser this will automate the process of creating newUser Object and adding properties and method reference.
 
 function createUser(name, score) {
     var newUser = Object.create(userFunctionsStore);
