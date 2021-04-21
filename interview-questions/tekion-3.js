d47c222bdc01495ea7b649caef3b81687ca4df6f
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2021-04-21 22:31:15 +0530

    Create tekion-3.js

---
+++ interview-questions/tekion-3.js
@@ -0,0 +1,22 @@
+// Create a Middleware that caches the api response of already existing response data
+
+const sampleRequest = (ids) => {
+	console.log('requested for', ids);
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(ids)) {
+      reject(new Error('fail'));
+    }
+    const response = ids.reduce((acc, id) => {
+      acc[id] = `${id}_test`
+      return acc;
+    }, {});
+    setTimeout(() => {
+      resolve(response);
+    }, 1000);
+  })
+}
+
+sampleRequest([1,2,3]).then(r => console.log(r));
+setTimeout(() => {
+	sampleRequest([1,2,3,4,5]).then(r => console.log(r));
+}, 2000);
