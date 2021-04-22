3fcfc82ffb4d784eb91ae734c07c354bdf141132
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2021-04-22 13:40:40 +0530

    Update tekion-3.js

--- interview-questions/tekion-3.js
+++ interview-questions/tekion-3.js
@@ -1,5 +1,42 @@
 // Create a Middleware that caches the api response of already existing response data
 
+function middleware(sampleResponse) {
+    let responseStore = {}; 
+
+    return function (ids) {
+        if(Array.isArray(ids)) {
+            let getDataIds = [];
+            ids.forEach((id) => {
+                if(!Object.keys(responseStore).includes(id)) {
+                    getDataIds.push(id);
+                }
+            });
+            return new Promise((resolve, reject) => {
+                sampleResponse(getDataIds).then(r => {
+                    responseStore = [...r, ...responseStore];
+                    resolve(responseStore);
+                });
+            })
+        }
+    }
+}
+
+// I will add time stamp when storing in store
+// sorted based on time stamp in store
+// Based on time ciriteria of data, I will expiry flag to data
+// And check the falag with time
+// Actual time
+// When accessed the data in store
+// There has to be timer that cleans based expiry
+// It should run always again problem
+// no, think...
+// some time slots - based on that I will clear memory which expiried
+// Based on the browser memory I will define the slots
+// How often it slows with meory growth
+// Based on data size in store  - these are not questions
+// How often data s cached - these are not questions to him
+// I will define the time slots based on data size and how often data is cached
+
 const sampleRequest = (ids) => {
 	console.log('requested for', ids);
   return new Promise((resolve, reject) => {
