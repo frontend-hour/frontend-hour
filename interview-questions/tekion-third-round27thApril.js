45581a5d04bb5c03ae81b5c2d76023d652f6f78c
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2021-04-28 15:26:31 +0530

    Update tekion-third-round27thApril.js

--- interview-questions/tekion-third-round27thApril.js
+++ interview-questions/tekion-third-round27thApril.js
@@ -1,4 +1,51 @@
+
+// call apply
+
+let sqaure = {
+    x: 4,
+    getArea: function() {
+        return this.side * this.side;
+    }
+}
+
+let sqaure2 = {
+    side: 5
+}
+
+sqaure.getArea.call(sqaure2)
+
+// add parameters to above
+
+let sqaure = {
+    x: 4,
+    getArea: function(a, b) {
+        return this.side * a * b;
+    }
+}
+
+let sqaure2 = {
+    side: 5
+}
+
+sqaure.getArea.call(sqaure2, 2 ,3);
+
 // BIND POLYFILL
+
+Function.prototype.mybind = function () {
+    const context = this;
+    const boundContext = arguments[0];
+    const boundArguments = Array.from(arguments).slice(1);
+
+    if (typeof context !== 'function') {
+        throw new TypeError('Not a valid function.')
+    }
+    return function () {
+        const allArgs = boundArguments.concat(Array.from(arguments));
+        context.apply(boundContext, allArgs);
+    }
+}
+
+let sqaureBind = sqaure.getArea.bind(sqaure2);
 
 // Debounce 
 function logName(name) {
