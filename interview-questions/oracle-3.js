5fc72a07aad940a0f5a9125e95dee12a2e78751a
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2021-04-28 15:20:58 +0530

    Update oracle-3.js

--- interview-questions/oracle-3.js
+++ interview-questions/oracle-3.js
@@ -1,5 +1,5 @@
 // 1. Call by value & call by reference
 // 2. Linked list
-// 3. Delete a node creatively
+// 3. Delete a node creatively when only one node is given (Ans: Change the current node to next and leave next node)
 // 4. What is a data structure and types of data structure
-// 5. JOINS in data tables
+// 5. JOINS in sql SELECT * FROM EMPLOYES, DEPARTMENTS
