f573c7fabb34fcfb2dfaca26a96ecefd9f696ed6
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2021-04-28 15:18:26 +0530

    Update oracle.js

--- interview-questions/oracle.js
+++ interview-questions/oracle.js
@@ -1,3 +1,4 @@
+//# 1 Impliment a power function that can take negative values as in exponent place
 function power(base, exp) {
   let total = 1;
   let sign = false;
@@ -5,31 +6,25 @@ function power(base, exp) {
     sign = true;
     exp = exp * -1;
   }
-  return rec(total, base, exp)
-//   for(let i = 1; i <= exp; i++) {
-//     total = total * base;
-//   }
-  
-  
   return sign ? 1/total : total;
 }
+console.log(power(2, -3));
 
-function rec(total, base, exp) {
-  if(!exp) {
-    return total;
-  } else {
-    return rec(total * base, base, exp -1);
-  }
-}
-
-
+//# 2 Impliment the same using recursion
 function power(base, exp) {
   return exp === 0 ? 1 : base * power(base, --exp);
 }
 
+// # 3 How do measure performance of a web page
+// For a customer page speed redcued, idenfity the problem
 
+// # 4
+// There hundred balls one ball is defective in these balls
+// All balls are manufactured equal in weight, but the defective ball weigh less
 // 100 balls
-// 1 defective ball - weight diff
+// 1 defective ball - weight less
+// given one common balance to indenfy the defective ball
+// In how many steps can you impliment the steps to weight 
 
 // 50 - 50 - 1
 // 25 - 25 - 2
@@ -37,5 +32,3 @@ function power(base, exp) {
 // 6 - 6 - 4
 // 3 - 3 - 5
 // 1 - 1  - 6 // 1 ball
-
-console.log(power(2, -3));
