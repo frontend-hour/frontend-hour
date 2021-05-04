dee2f50a0b892332c9700df58da8553291a2ae05
Raghu <konidelaraghu@gmail.com>
2021-05-04 23:12:38 +0100

    added alternate solution for Running sum problem

--- code-challenges/running-sum/running-sum.js
+++ code-challenges/running-sum/running-sum.js
@@ -1,9 +1,19 @@
-var runningSum = function(nums) {
+var runningSum = function (nums) {
     let p = [];
     let total = 0;
-    for(let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         total = total + nums[i];
         p.push(total);
     }
     return p;
+};
+
+
+//Solution 1 - Alternate solution
+
+var runningSum = function (nums) {
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1]
+    }
+    return nums
 };
