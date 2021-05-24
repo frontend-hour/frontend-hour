16d2c4e9dbb02d0ed6546d964d86416e01af04f5
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2021-05-24 22:09:07 +0530

    Create array-in-given-order.js

---
+++ code-challenges/array-in-given-order/array-in-given-order.js
@@ -0,0 +1,9 @@
+var createTargetArray = function(nums, index) {
+    let res = [];
+    
+    for(let i = 0; i < nums.length; i++) {
+        res.splice(index[i], 0, nums[i]);
+    }
+    
+    return res;
+};
