0739b9f7c814aa47ef5c6688f6ec10fe63e3c6d6
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2022-02-12 11:14:25 +0530

    Update letter-combinations-of-phone-number.js
    letter comibations using back tracking

--- code-challenge/letter-combinations-of-phone-number/letter-combinations-of-phone-number.js
+++ code-challenge/letter-combinations-of-phone-number/letter-combinations-of-phone-number.js
@@ -1,48 +1,32 @@
-//TODO: Not working for more than 2 digit inputs
-// Work in Progress
-
 /**
  * @param {string} digits
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    let phoneKeyPad = {
-        2 : 'abc',
-        3 : 'def',
-        4 : 'ghi',
-        5 : 'jkl',
-        6 : 'mno',
-        7 : 'pqrs',
-        8 : 'tuv',
-        9 : 'wxyz'
+    let result = [];
+    const digitMap = {
+        '2' : ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
     }
     
-    if(digits.length) {
-        if(digits.length === 1) {
-            return [...phoneKeyPad[digits]];
-        } else {
-            let s = [];
-            for(let i = 0; i < digits.length; i++) {
-                s.push(phoneKeyPad[digits[i]]);
-            }
-            let r = [];
-            for(let j = 0; j < s.length; j++) {
-                for(let k = 0; k < s[j].length; k++) {
-                    for(let i = 0; i < s.length; i++) {
-                        if(j !== i) {
-                            console.log(s[j][k], s[i]);
-                            for(let p = 0; p < s[i].length; p++) {
-                                if(!r.includes(s[j][k] + s[i][p]) && !r.includes(s[i][p] + s[j][k]))
-                                r.push(s[j][k] + s[i][p]);
-                            }
-                        }
-                    }
-                }
-            }
-            console.log(r);
-            return r;
+    function backTrack(index, combinationStr) {
+        if(combinationStr.length === digits.length) {
+            result.push(combinationStr);
+            return;
         }
-    } else {
-        return [];
+        let currentTarget = digitMap[digits[index]];
+        for(let i = 0; i < currentTarget.length; i++) {
+            backTrack(index + 1, combinationStr + currentTarget[i]);
+        }
     }
+    if(!digits)
+        return [];
+    backTrack(0, "");
+    return result;
 };
