1b4b4bdbaca03c4c7e4d967167d05989e963b97d
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2022-05-08 12:50:23 +0530

    Create promise-polyfill.js

---
+++ concepts/promise-polyfill.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+
+class myPromise {
+  constructor(func) {
+    this.value;
+    this.state = "pending";
+
+    this.resolve = this.resolve.bind(this);
+    this.reject = this.reject.bind(this);
+
+    func(this.resolve, this.reject);
+  }
+
+  resolve(data) {
+    this.state = "resolved";
+    this.thenCallback ? this.thenCallback(data) : "";
+  }
+
+  reject(error) {
+    this.state = "rejected";
+    this.catchCallback ? this.catchCallback(error) : "";
+  }
+
+  then(thenCB) {
+    this.thenCallback = thenCB;
+  }
+
+  catch(catchDB) {
+    this.catchCallback = catchDB;
+  }
+}
+
+let s = new myPromise((resolve, rejected) => {
+  setTimeout(() => {
+    resolve("my promise");
+  }, 3000);
+});
+
+s.then((data) => {
+  console.log(data);
+});
