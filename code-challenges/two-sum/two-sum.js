c6c12090c5f2d0f88ccdac15f9e48d58f1c76e73
Chaitanya Kumar <chaitanya.kmc@gmail.com>
2022-07-16 10:52:25 +0530

    Update two-sum.js
    Added best solution

--- code-challenges/two-sum/two-sum.js
+++ code-challenges/two-sum/two-sum.js
@@ -3,6 +3,25 @@
  * @param {number} target
  * @return {number[]}
  */
+// Best Solution
+var twoSum = function(nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (target - nums[i] in map) {
+            return [map[target-nums[i]], i];
+        } else {
+            map[nums[i]] = i;
+        }
+    }
+};
+
+
+// Secondary Solution
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
